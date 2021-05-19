04e5ef7bcceaac2162d1d3b48d4be52d90629641
Bradley Jackson <bradley.jackson@ARQ66766.local>
2021-05-19 11:58:58 +1000

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -15,15 +15,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # He sees the page title & header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         # He is prompted to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # He types "Buy batteries" into a text box.
+        inputbox.send_keys('Buy batteries')
 
         # Upon hitting enter, the page updates, and now the page lists
         # "1: Buy batteries" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy batteries' for row in rows)
+        )
+        #There's still a text box inviting her to add another item.
+        #She enters "put batteries into remote"
+        self.fail('Finish the test!')
         # Steve wraps-up his list building
+
 
 if __name__ == '__main__':
     unittest.main()
