e9ff4d8ddb21f3507ef2728c6aef18b6528dfba7
Bradley Jackson <bradley.jackson@ARQ66766.local>
2021-05-19 10:27:03 +1000

    Refactored into unit testing!

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Steve is looking to use an online to-do app. He goes to the homepage.
+        browser.get('http://localhost:8000')
+
+        # He sees the page title & header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        # He is prompted to enter a to-do item straight away
+
+        # He types "Buy batteries" into a text box.
+
+        # Upon hitting enter, the page updates, and now the page lists
+        # "1: Buy batteries" as an item in a to-do list
+
+        # Steve wraps-up his list building
+
+if __name__ == '__main__':
+    unittest.main()
