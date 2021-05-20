5afb06bdb9c51fd1ed6ff226457eea896cd1473e
Bradley Jackson <bradley.jackson@ARQ66766.local>
2021-05-20 13:55:28 +1000

    refactor functional_tests as a django test app

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -11,9 +11,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Steve is looking to use an online to-do app. He goes to the homepage.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He sees the page title & header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -31,10 +36,9 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy batteries" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy batteries')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy batteries', [row.text for row in rows])
+
         #There's still a text box inviting him to add another item.
         #he enters "put batteries into remote"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -43,12 +47,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         #The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy batteries', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy batteries')
+        self.check_for_row_in_list_table('2: put batteries into remote')
 
         #Edith wonders whether the site will remember her list.
         #She sees that the site has generated a unique URL for her.
